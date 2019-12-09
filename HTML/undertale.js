d75d3a050e4fb3d1ec3275fe30836a8c089282e6
Benjamin Hill <benjaminhill499@gmail.com>
2019-12-09 05:14:07 -0500

    DOG BARK FINAL

--- HTML/undertale.js
+++ HTML/undertale.js
@@ -1,8 +1,14 @@
+
+
 function onHover()
 {
     $("#dog").attr('src', 'images/dog-bark.png');
-}
 
+}
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
 function offHover()
 {
     $("#dog").attr('src', 'images/dog.png');
